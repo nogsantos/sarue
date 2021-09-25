f0b06ab24b89565c328e547a24525ce842e88015
Fabricio Nogueira <fabricio.nogueira@instruct.com.br>
2021-09-25 09:47:47 -0300

    style(license): adds license header to files

--- platform/github.go
+++ platform/github.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2021 The Sarue Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package platform
 
 import (
@@ -46,7 +62,6 @@ func (gb *Github) createConfDir() {
 
 func (gb *Github) ParseFile(generate *application.Generate) {
 	gb.ConfigFile = strings.ToLower(generate.Language.Name)
-	// fmt.Printf("TEST1 %v \n",gb.ConfigFile) // REMOVE
 	gb.configureFile(generate)
 }
 
@@ -59,7 +74,6 @@ func (gb *Github) configureFile(generate *application.Generate) {
 			} else if conf == "test" {
 				tartargetConf = gb.GithubTestTemplate()
 			}
-			// fmt.Printf("TEST %v \n",gb.ConfigFile) // REMOVE
 			utils.WriteConfigFile(gb.ConfigDir, gb.ConfigFile+"."+conf + ".yaml", tartargetConf, generate)
 		}
 	}
